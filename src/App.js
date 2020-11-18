d8f3fdbdc08e205bcb7048c1a5cdf507e0c349ea
ayushyadav468 <ayushyadav468@gmail.com>
2020-11-18 21:42:53 +0530

    used two binding in App.js and Person.js

--- src/App.js
+++ src/App.js
@@ -11,15 +11,40 @@ class App extends Component {
     ],
   };
 
+  switchNameHandler = (newName) =>
+    this.setState({
+      person: [
+        { name: newName, age: 21 },
+        { name: 'Aryaman', age: 18 },
+        { name: 'Kavita', age: 42 },
+      ],
+    });
+
+  nameChangeHandler = (event) =>
+    this.setState({
+      person: [
+        { name: 'Ayush', age: 21 },
+        { name: event.target.value, age: 18 },
+        { name: 'Kavita', age: 42 },
+      ],
+    });
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am from App.js file</h1>
+        <button onClick={() => this.switchNameHandler('Ayush Yadav')}>
+          Switch Button
+        </button>
         <Person
           name={this.state.person[0].name}
           age={this.state.person[0].age}
         ></Person>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}>
+        <Person
+          name={this.state.person[1].name}
+          age={this.state.person[1].age}
+          change={this.nameChangeHandler}
+        >
           <li>I am younger than Ayush</li>
         </Person>
         <Person
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Person.css';
 
-const person = (props) => {
+const Person = (props) => {
   return (
     <div>
-      <p>
+      <p onClick={props.click}>
         My name is {props.name}. I am {props.age} old.
       </p>
       <ul>{props.children}</ul>
+      <input type="text" placeholder={props.name} onChange={props.change} />
     </div>
   );
 };
 
-export default person;
+export default Person;
